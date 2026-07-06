9f3e3687ff39d2bc03f6e7f3bf3a119e5157874a
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-07-06 16:04:19 +0800

    feat: add personal access tokens migration and update Sanctum middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
