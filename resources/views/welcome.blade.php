5cb447e57b77fc9b2a1c958695a0c06e4bba9c4c
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-07-14 12:37:20 +0800

    feat: add favicon link to layout files for consistent branding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="shortcut icon" href="{{ asset('logo-tebarkode.png') }}" type="image/x-icon">
+
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
