4f32a9dee1f48f5f35643f2557bad34ad2974315
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-07-22 18:47:40 +0800

    fix: update root route to redirect to login instead of displaying welcome view

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+// Route::view('/', 'welcome');
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
